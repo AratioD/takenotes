cf5e23aa98872aad5541dea4d71c78ac77a8a4f0
AratioD <fffb@ddd.com>
2020-04-16 21:50:49 +0300

    first comments

---
+++ app.js
@@ -0,0 +1,10 @@
+const getNotes = require('./notes.js')
+
+const msg = getNotes()
+
+console.log(msg)
+
+// const add = require('./utils.js')
+
+// const sum = add(3,4)
+// console.log(sum)
---
+++ notes.js
@@ -0,0 +1,5 @@
+const getNotes = function () {
+    return 'my notes areee'
+}
+
+module.exports = getNotes
