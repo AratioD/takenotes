d61e228ee26c08bafa2a55b8485eaa3b69bc25cb
AratioD <fffb@ddd.com>
2020-05-02 19:07:51 +0300

    i can fetch one doctor

--- src/index.js
+++ src/index.js
@@ -8,15 +8,46 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 // reserve an appointment to doctor
 app.post('/doctor', (req, res) => {
-   
+
     const doctor = new Doctor(req.body)
 
     doctor.save().then(() => {
-        res.send(doctor)
+        res.status(200).send(doctor)
     }).catch((e) => {
         res.status(400).send(e)
     })
 })
+
+// get all users
+app.get('/doctor', (req, res) => {
+
+    Doctor.find({}).then((doctors) => {
+        res.send(doctors)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })
+})
+
+//get a specific users
+app.get('/doctor/:id', (req, res) => {
+
+    const _id = req.params.id
+    Doctor.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+    // console.log(req.params)
+    // Doctor.find({}).then((doctors) => {
+    //     res.send(doctors)
+    // }).catch((e) => {
+    //     res.status(500).send(e)
+    // })
+})
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
