a8abd10aac065d14b62f9e8e2ed29f763e836c49
AratioD <fffb@ddd.com>
2020-05-02 10:57:45 +0300

    added a separate model file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,31 +6,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/book-doctor-api'), {
     useCreateIndex: true
 }
 
-// Doctor profession and time slot
-const Doctor = mongoose.model('Doctor', {
-
-    profession: {
-        type: String,
-        require: true,
-        trim: true
-
-    },
-    name: {
-        type: String,
-        require: true,
-        trim: true,
-        lowercase: true,
-    },
-    time: {
-        type: Number,
-        require: true,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Time slot cannot be negative')
-            }
-        }
-    }
-})
 
 // // Doctor profession and time slot
 // const Doctor = mongoose.model('Doctor', {
@@ -47,14 +22,14 @@ const Doctor = mongoose.model('Doctor', {
 // })
 
 
-const test = new Doctor({
-    profession: 'yleislaakari',
-    name: 'jimo',
-    time: 1
-})
+// const test = new Doctor({
+//     profession: 'yleislaakari',
+//     name: 'jimo',
+//     time: 1
+// })
 
-test.save().then(() => {
-    console.log(test)
-}).catch((error) => {
-    console.log('error', error)
-})
+// test.save().then(() => {
+//     console.log(test)
+// }).catch((error) => {
+//     console.log('error', error)
+// })
---
+++ src/models/doctor.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+
+// Doctor profession and time slot
+const Doctor = mongoose.model('Doctor', {
+
+    profession: {
+        type: String,
+        require: true,
+        trim: true
+
+    },
+    name: {
+        type: String,
+        require: true,
+        trim: true,
+        lowercase: true,
+    },
+    time: {
+        type: Number,
+        require: true,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Time slot cannot be negative')
+            }
+        }
+    }
+})
