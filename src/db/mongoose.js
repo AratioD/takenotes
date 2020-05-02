5f4c7c908bda3a3b49296f0561d482b2b2334bd2
AratioD <fffb@ddd.com>
2020-05-02 10:36:58 +0300

    mongoose validation added and deleted old file db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/book-doctor-api'), {
     useNewUrlParser: true,
@@ -9,19 +10,46 @@ mongoose.connect('mongodb://127.0.0.1:27017/book-doctor-api'), {
 const Doctor = mongoose.model('Doctor', {
 
     profession: {
-        type: String
+        type: String,
+        require: true,
+        trim: true
+
     },
     name: {
-        type: String
+        type: String,
+        require: true,
+        trim: true,
+        lowercase: true,
     },
     time: {
-        type: Number
+        type: Number,
+        require: true,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Time slot cannot be negative')
+            }
+        }
     }
 })
 
+// // Doctor profession and time slot
+// const Doctor = mongoose.model('Doctor', {
+
+//     profession: {
+//         type: String
+//     },
+//     name: {
+//         type: String
+//     },
+//     time: {
+//         type: Number
+//     }
+// })
+
+
 const test = new Doctor({
     profession: 'yleislaakari',
-    name: 'simo',
+    name: 'jimo',
     time: 1
 })
 
