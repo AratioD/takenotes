7389d8f4c6fe07bb04b057a1b66401a4978db6a6
AratioD <fffb@ddd.com>
2020-05-03 15:41:17 +0300

    removed unused names

--- src/index.js
+++ src/index.js
@@ -48,9 +48,14 @@ app.get('/doctor/:id', async (req, res) => {
 
 //Fetch a specific doctor all time slots.
 app.get('/doctorname', async (req, res) => {
-
     try {
         const doctor = await Doctor.find(req.body)
+
+        const name = req.body
+        if (doctor.length==0) {
+            res.send("No such a name: " + req.body.name)
+        }
+
         res.send(doctor)
     } catch (error) {
         res.status(500).send(error)
