c1410c0b01c74f33e2a0c1ea1c5ebda9aa1aa438
AratioD <fffb@ddd.com>
2020-05-03 16:59:47 +0300

    added delete functionality there

--- src/index.js
+++ src/index.js
@@ -38,11 +38,11 @@ app.get('/doctor/:id', async (req, res) => {
     try {
         const doctor = await Doctor.findById(_id)
         if (!doctor) {
-            return res.status(404).send()
+            return res.status(404).send("not found user by following id --> " + _id)
         }
         res.send(doctor)
     } catch (error) {
-        res.status(500).send()
+        res.status(500).send(error)
     }
 })
 
@@ -52,13 +52,28 @@ app.get('/doctorname', async (req, res) => {
         const doctor = await Doctor.find(req.body)
 
         const name = req.body
-        if (doctor.length==0) {
+        if (doctor.length == 0) {
             res.send("No such a name: " + req.body.name)
         }
 
         res.send(doctor)
     } catch (error) {
         res.status(500).send(error)
+    }
+})
+
+//Delete one reservation there
+app.delete('/remove/:id', async (req, res) => {
+    const _id = req.params.id
+
+    try {
+        const doctor = await Doctor.deleteOne({ "_id": (_id) })
+        if (!doctor) {
+            return res.status(404).send(_id)
+        }
+        res.send("Succesfully deleted -->" + _id)
+    } catch (error) {
+        res.status(500).send(error + " " + " " + _id)
     }
 })
 
