c4f456b79aca95b9374d90f1138be8fe50fbe4c1
AratioD <fffb@ddd.com>
2020-05-04 21:21:21 +0300

    added db schema pre values, to be tested

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,6 +6,6 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-mongoose.connect('mongodb://127.0.0.1:27017/book-doctor-api')
+mongoose.connect('mongodb://127.0.0.1:27017/maintenance-book-api')
 
 
---
+++ src/models/manual.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')   
+
+// Model for a Manual profession and time slot
+const Manual = mongoose.model('Manual', {
+
+
+    urgency: {
+        type: String, enum: ['high', 'middle', 'low'],
+        require: true,
+        trim: true
+
+    },
+    name: {
+        type: String,
+        require: true,
+        trim: true,
+        lowercase: true,
+    },
+    time: {
+        type: Number,
+        require: true,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Time slot cannot be negative')
+            }
+        }
+    }
+})
+
+module.exports = Manual
